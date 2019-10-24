08c7667daab268c6e588394a1c4db4d5c9fe034f
brunostranak <41579737+brunostranak@users.noreply.github.com>
2019-10-24 07:54:04 -0300

    Update addlivro.php

--- LeiturasCompartilhadas/login/index2/addlivro.php
+++ LeiturasCompartilhadas/login/index2/addlivro.php
@@ -12,7 +12,7 @@ $resumo= strip_tags($_POST["resumo"]);
 
 
 
- move_uploaded_file($imagem["tmp_name"],"D:/wamp64/www/PJI/LeiturasCompartilhadas/login/imagens/".$imagem["name"]);
+ move_uploaded_file($imagem["tmp_name"],"C:/wamp64/www/PJI/LeiturasCompartilhadas/login/imagens/".$imagem["name"]);
  
  
 $sql="INSERT INTO livros (idUser,nomelivro,autor,editora,status,resumo,imagem)
