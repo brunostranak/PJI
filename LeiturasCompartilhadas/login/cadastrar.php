53b2110fcdc41e41ff8a12d3c5add391bf239a47
1731343 <1731343@LABFMAQ16.itp.local>
2020-02-14 08:38:05 -0300

    data,curso e genero adicionados

--- LeiturasCompartilhadas/login/cadastrar.php
+++ LeiturasCompartilhadas/login/cadastrar.php
@@ -7,13 +7,17 @@ $cnx= conexao();
 
 $nome= strip_tags($_POST["nome"]);
 $email=strip_tags($_POST["email"]);
+$dtNasc= strip_tags($_POST["dtNasc"]);
+$genero = strip_tags($_POST["gender"]);
+$curso = strip_tags($_POST["curso"]);
 $senha=strip_tags($_POST["senha"]);
 $telefone=strip_tags($_POST["telefone"]);
 
 
 
-$sql = "INSERT INTO usuarios (nomeUser,email,senha,telefone) 
- VALUES ('$nome','$email','$senha','$telefone')";
+$sql = "INSERT INTO usuarios (nomeUser, dtNasc,genero,curso,email,senha,telefone) 
+ VALUES ('$nome', '$dtNasc', '$genero','$curso','$email','$senha','$telefone')";
+
 
 $sql2= "SELECT * FROM usuarios WHERE email = '$email'";
 $resultadoverific=mysqli_query($cnx,$sql2);
--- LeiturasCompartilhadas/login/paginacadastro.php
+++ LeiturasCompartilhadas/login/paginacadastro.php
@@ -21,7 +21,9 @@ session_start();
   <title>Flat Login Form</title>
   
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900'>
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'>
 <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
@@ -42,13 +44,14 @@ session_start();
   </div>
 </div>
     
-<div class="form">
+<div class="form" >
     
   <div class="thumbnail"><img src="https://image.flaticon.com/icons/svg/201/201556.svg"/></div>
   
   <form action="cadastrar.php" method="post">
       
     <input required type="text" placeholder="Nome e Sobrenome" name="nome"/>
+    <input required placeholder="Data de Nascimento" name="dtNasc" class="textbox-n" type="text" onfocus="(this.type='date')"  id="date">
     <input required type="email" placeholder="Email" name="email"/>
    
 
@@ -67,6 +70,39 @@ session_start();
 }
 </script>
     <input onkeypress="Mascara(this);" required type="text"  placeholder="Celular" name="telefone" maxlength="15"/>
+    
+    
+    
+    <select required class="form-control" required id="curso" name="curso">
+        <option value="info1">1º ano Integrado Informática</option>
+        <option value="info2">2º ano Integrado Informática</option>
+        <option value="info3">3º ano Integrado Informática</option>
+        <option value="info4">4º ano Integrado Informática</option>
+        <option value="eletro1">1º ano Integrado Eletromecânica</option>
+        <option value="eletro2">2º ano Integrado Eletromecânica</option>
+        <option value="eletro3">3º ano Integrado Eletromecânica</option>
+        <option value="eletro4">4º ano Integrado Eletromecânica</option>
+        <option value="outro">Outro</option>
+    </select>
+           
+    
+    <br>
+    
+    <label for="male">Masculino</label>
+    
+    <input style="width:20px;" type="radio" id="male" name="gender" value="male">
+    
+    
+    <br>
+    <label for="female">Feminino</label>
+    
+    <input style="width:20px;" type="radio" id="female" name="gender" value="female">
+    
+    <br>
+    
+    <label for="other">Outro</label>
+    
+    <input style="width:20px;" type="radio" id="other" name="gender" value="other">
     
     
     <button type="submit">Cadastrar</button>
